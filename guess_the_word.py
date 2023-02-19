9f5e3f5e88cd70d6444e86edf98b92c57eeaa035
Ivo Petkov <48838737+ip681@users.noreply.github.com>
2023-02-19 18:56:11 +0200

    Create guess_the_word.py

---
+++ guess_the_word.py
@@ -0,0 +1,31 @@
+import random
+words = ['cat', 'computer', 'coffee']
+word = random.choice(words)
+print("Guess the word")
+guesses = ""
+wrong_letters = ""
+turns = 12
+while turns > 0:
+    failed = 0
+    for char in word:
+        if char in guesses:
+            print(char, end=" ")
+        else:
+            print("_", end=" ")
+            failed += 1
+    print("")
+    print("__________________________________________________________________")
+    if failed == 0:
+        print("You Win!")
+        print("The word is: ", word)
+        break
+    print()
+    guess = input("Input letter:")
+    guesses += guess
+    if guess not in word:
+        turns -= 1
+        print("Wrong! You have", + turns, 'more guesses')
+        wrong_letters += guess + " "
+        if turns == 0:
+            print("You Loose!")
+    print(f"Wrong letters are: {wrong_letters}")
