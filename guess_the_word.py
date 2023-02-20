b09cc59b329c2897d7fcc63004a8906c501bd924
Ivo Petkov <48838737+ip681@users.noreply.github.com>
2023-02-20 21:41:49 +0200

    Update guess_the_word.py

--- guess_the_word.py
+++ guess_the_word.py
@@ -17,7 +17,7 @@ man = ["________ \n| \n| \n| \n| \n| \n| \n|____________",
 
 word = random.choice(words)
 guesses = ""
-wrong_letters = ""
+wrong_guesses = ""
 turns = 12
 
 print("Guess the word")
@@ -42,16 +42,15 @@ while turns > 0:
         if len(guess) == 1 and guess.isalpha(): # check for more than one character and not letter character
             guess = guess.lower() # make the letter lowercase if it is uppercase
             break
-
         else:
             print("Enter a single letter (a-z).")
             continue
     guesses += guess
-    if guess not in word:
+    if guess not in word: #checks if the letter entered is part of a word
         turns -= 1
         print(f"Wrong! You have {turns} more guesses")
-        wrong_letters += guess + " "
+        wrong_guesses += guess + " "
         if turns == 0:
             print("You Loose!")
-    print(f"Wrong letters are: {wrong_letters}")
+    print(f"Wrong letters are: {wrong_guesses}")
     print(man[12 - turns])
