abcf37ed6ae1a43103b17f711f99dbf4941df73b
Ivo Petkov <48838737+ip681@users.noreply.github.com>
2023-02-25 23:07:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,18 +4,10 @@ from termcolor import colored
 current_theme = ""
 theme_name = ""
 words = ""
-
-theme_animals = ['cat', 'dog', 'rat']
-theme_jobs = ['programmer', 'analyst']
-
-theme_index = random.choice(range(0, 2))
-
-if theme_index == 0:
-    theme_name = "animals"
-    words = theme_animals
-elif theme_index == 1:
-    theme_name = "jobs"
-    words = theme_jobs
+guesses = ""
+wrong_guesses = ""
+MAX_TURNS = 12
+turns = 12
 
 man = ["________ \n| \n| \n| \n| \n| \n| \n|____________",
        "________ \n|      | \n| \n| \n| \n| \n| \n|____________",
@@ -31,17 +23,47 @@ man = ["________ \n| \n| \n| \n| \n| \n| \n|____________",
        "________ \n|      | \n|      O \n|     /|\\ \n|    / | \\ \n|     / \\ \n|    / \n|____________",
        "________ \n|      | \n|      O \n|     /|\\ \n|    / | \\ \n|     / \\ \n|    /   \\ \n|____________"]
 
+theme_animals = ['cat', 'dog', 'monkey', 'cow', 'crocodile', 'elephant', 'lion', 'tiger', 'giraffe', 'zebra']
+theme_birds = ['raven', 'eagle', 'duck', 'sparrow', 'pigeon', 'parrot', 'seagull', 'pelican', 'flamingo', 'marabou']
+theme_jobs = ['programmer', 'analyst', 'actor', 'singer', 'engineer', 'architect', 'barber', 'scientist', 'lawyer', 'designer']
+theme_body_parts = ['head', 'neck', 'arm', 'foot', 'leg', 'hand', 'chest', 'knee', 'elbow', 'ankle']
+
+theme_index = random.choice(range(0, 4))
+
+if theme_index == 0:
+    theme_name = "animals"
+    words = theme_animals
+elif theme_index == 1:
+    theme_name = "birds"
+    words = theme_birds
+elif theme_index == 2:
+    theme_name = "jobs"
+    words = theme_jobs
+elif theme_index == 3:
+    theme_name = "parts of body"
+    words = theme_body_parts
+
 word = random.choice(words)
-guesses = ""
-wrong_guesses = ""
-MAX_TURNS = 12
-turns = 12
+
+man = ["________ \n| \n| \n| \n| \n| \n| \n|____________",
+       "________ \n|      | \n| \n| \n| \n| \n| \n|____________",
+       "________ \n|      | \n|      O \n| \n| \n| \n| \n|____________",
+       "________ \n|      | \n|      O \n|      | \n| \n| \n| \n|____________",
+       "________ \n|      | \n|      O \n|      | \n|      | \n| \n| \n|____________",
+       "________ \n|      | \n|      O \n|     /| \n|      | \n| \n| \n|____________",
+       "________ \n|      | \n|      O \n|     /| \n|    / | \n| \n| \n|____________",
+       "________ \n|      | \n|      O \n|     /|\\ \n|    / | \n| \n| \n|____________",
+       "________ \n|      | \n|      O \n|     /|\\ \n|    / | \\ \n| \n| \n|____________",
+       "________ \n|      | \n|      O \n|     /|\\ \n|    / | \\ \n|     / \n| \n|____________",
+       "________ \n|      | \n|      O \n|     /|\\ \n|    / | \\ \n|     / \n|    / \n|____________",
+       "________ \n|      | \n|      O \n|     /|\\ \n|    / | \\ \n|     / \\ \n|    / \n|____________",
+       "________ \n|      | \n|      O \n|     /|\\ \n|    / | \\ \n|     / \\ \n|    /   \\ \n|____________"]
 
 print(f"Guess the word! Theme is {theme_name.upper()}")
 while turns > 0:
 
-    print("____________________________________________________________________\n")
     failed = 0
+
     for char in word:
         if char in guesses:
             print(char, end=" ")
@@ -50,20 +72,22 @@ while turns > 0:
             failed += 1
 
     print("")  # new line
+
     if failed == 0:
         print(colored("You Win!", "green"))
-        print(f"The word is: {word}")
+        print(f"The word is: {word.upper()}")
         break
 
     print()
 
     while True:
+        print("____________________________________________________________________\n")
 
         guess = input("Input letter: ")
 
-        # if guesses.find(str(guess)) != -1:
-        #     print(f"You already try the letter {guess}")
-        #     turns += 1
+        if guesses.find(str(guess)) != -1: # if repeat wrong guess don't lose try and output message
+            print(f"You already try the letter {guess}")
+            turns += 1
 
         if len(guess) == 1 and guess.isalpha():  # check for more than one character and not letter character
             guess = guess.lower()  # make the letter lowercase if it is uppercase
@@ -81,6 +105,8 @@ while turns > 0:
 
         if turns == 0:
             print(colored("You Loose!", "red"))  # color text. wait fot test in cmd
+            print(f"The word is: {word.upper()}")
 
+    print(f'Theme is {theme_name.upper()}')
     print(f"Wrong letters are: {wrong_guesses}")
     print(man[MAX_TURNS - turns])
